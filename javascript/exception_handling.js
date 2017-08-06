8a0c1bf53b8b0bcd976b71a1d8a70b16774c8101
Arakawa Shintaro <arashinbe@gmail.com>
2017-08-06 22:45:36 +0900

    exception handling

---
+++ javascript/exception_handling.js
@@ -0,0 +1,37 @@
+//throw
+// UserException というオブジェクト型を作成
+function UserException (message){
+  this.message=message;
+  this.name="UserException";
+}
+
+// 文字列として使用されるとき（例 : エラーコンソール上）に
+// 例外を整形する
+UserException.prototype.toString = function (){
+  return this.name + ': "' + this.message + '"';
+}
+
+// UserException のインスタンスを作成し、それをスローする
+throw new UserException("Value too high");
+
+
+//catch~try~(finnaly~)
+try {
+  hoge;
+} catch(e) {
+  console.log(e.message);
+  console.log(e.name);
+  console.log(e.constructor);
+} finally {
+  console.log('hoge');
+}
+
+
+//throwではエラーオブジェクトのインスタンスでなくてもおk
+try {
+  throw 'hoge';
+} catch(e) {
+  console.log(e); //hoge
+  console.log(e.message); //undefined
+  console.log(e instanceof Error); //false
+}
