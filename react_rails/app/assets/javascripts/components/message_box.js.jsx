b44cca016e4b1202f2b35dacb29d7db2d48f8b9e
Arakawa Shintaro <arashinbe@gmail.com>
2017-08-09 01:08:18 +0900

    change null array

--- react_rails/app/assets/javascripts/components/message_box.js.jsx
+++ react_rails/app/assets/javascripts/components/message_box.js.jsx
@@ -5,13 +5,8 @@ var MessageBox = React.createClass({
   */
 
    getInitialState: function() {
-    return {
-      messages: [
-        { id: 1, user: 'Tom',   text: 'Good morning' },
-        { id: 2, user: 'John',  text: 'Good afternoon' },
-        { id: 3, user: 'Emily', text: 'Good evening' }
-      ]
-    };
+    return { messages: [] };
+  },
 
   /*
   サーバーからメッセージ一覧を取得し、
