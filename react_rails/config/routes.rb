973f629e292621bd8a717cb352ea1c3c4df1b01a
Arakawa Shintaro <arashinbe@gmail.com>
2017-08-09 00:35:21 +0900

    create Messages API

--- react_rails/config/routes.rb
+++ react_rails/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  resources :messages, only: [:index, :create], format: 'json'
   root to: 'top#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
