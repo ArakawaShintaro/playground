ff2f60b43cbfbe03760170d67ea939ebc03b0850
Arakawa Shintaro <arashinbe@gmail.com>
2017-08-12 23:25:02 +0900

    dispatch action
    ActionCreatorとviewをつないだ

--- flux_react/index.js
+++ flux_react/index.js
@@ -1,7 +1,44 @@
 import { createStore } from 'redux';
+import { addTodo, toggleTodo, setVisibilityFilter } from './actions/index.js';
 
 //createStoreの引数はReducer
 let store = createStore(function() {
   return 'Hello redux';
 });
 
+//TODOの追加
+var addTodoElem = document.getElementById('addTodo');
+console.log(addTodoElem);
+var input = addTodoElem.getElementsByTagName('input')[0];
+console.log(input);
+var button = addTodoElem.getElementByTagName('button')[0];
+
+button.addEventListener('click', () => {
+  //Storeにアクションを渡す
+  var todoText = input.value;
+  store.dispatch(addTodo(todoText));
+});
+
+//TOODの完了
+var todoList = document.getElementById('todoList');
+var elements = todoList.getElementByTagName('li');
+console.log(elements);
+var listArray = [...elements];
+console.log(listArray);
+
+listArray.forEach((v, index) => {
+  v.addEventListener('click', e => {
+    store.dispatch(toggleTodo(index));
+  });
+});
+
+//フィルタリング
+var links = document.getElementById('links');
+var childs = links.childrenNodes;
+var childList = [...childs];
+childList.filter(v => v.nodeName != '#text').forEach(v => {
+  v.addEventListener('click', e => {
+    var filterText = v.innerHTML;
+    store.dispatch(setVisibilityFilter(filterText));
+  });
+});
