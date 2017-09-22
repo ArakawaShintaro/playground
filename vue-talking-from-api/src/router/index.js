86d0d40ae81cecc86361cbff2032f568d4db58a1
Arakawa Shintaro <arashinbe@gmail.com>
2017-09-22 16:58:58 +0900

    add routing

--- vue-talking-from-api/src/router/index.js
+++ vue-talking-from-api/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Top from '@/components/Top'
+import Talk from '@/components/Talk'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Top',
+      component: Top
+    },
+    {
+      path: '/talk',
+      name: 'Talk',
+      component: Talk
     }
   ]
 })
