98dd65c611bccd6d3be8a44ac02d15779cd2cabf
Arakawa Shintaro <arashinbe@gmail.com>
2017-09-22 16:58:50 +0900

    import neaded lib

--- vue-talking-from-api/src/main.js
+++ vue-talking-from-api/src/main.js
@@ -1,9 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-default/index.css'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// import '../settings.js'
+
+Vue.use(ElementUI, {locale})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
