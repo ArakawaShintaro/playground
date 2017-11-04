91bae8d45a6127eeb058776852a3741a56fafdbb
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-04 20:35:02 +0900

    exampleとテストの中身が被ってるので、exampleを消去した

--- rspec/rspec3-for-beginners/message_filter_spec.rb
+++ rspec/rspec3-for-beginners/message_filter_spec.rb
@@ -1,16 +1,16 @@
 require_relative 'message_filter'
 
-describe MessageFilter do
+describe MessageFilter, 'with argument "foo"'  do
   before do
     @filter = MessageFilter.new('foo')
   end
-  it 'detects message with NG word' do
+  it {
     # expect(@filter.detect?('hello from foo')).to eq true
     expect(@filter).to be_detect('hello from foo')
-  end
+  }
 
-  it 'does not detect message without NG word' do
+  it {
     # expect(@filter.detect?('hello, world!')).to eq false
     expect(@filter).not_to be_detect('hello, world')
-  end
+  }
 end
