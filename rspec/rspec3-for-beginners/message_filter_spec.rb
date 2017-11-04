92489cc0c3daf19d3b5497ec7cb2e6e67009a6ba
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-04 20:39:56 +0900

    改行をなくした

--- rspec/rspec3-for-beginners/message_filter_spec.rb
+++ rspec/rspec3-for-beginners/message_filter_spec.rb
@@ -2,13 +2,8 @@ require_relative 'message_filter'
 
 describe MessageFilter, 'with argument "foo"'  do
   subject { MessageFilter.new('foo') }
-  it {
-    # expect(@filter.detect?('hello from foo')).to eq true
-    is_expected.to be_detect('hello from foo')
-  }
-
-  it {
-    # expect(@filter.detect?('hello, world!')).to eq false
-    is_expected.not_to be_detect('hello, world')
-  }
+  it { is_expected.to be_detect('hello from foo') }
+  # expect(@filter.detect?('hello from foo')).to eq true
+  it { is_expected.not_to be_detect('hello, world') }
+  # expect(@filter.detect?('hello, world!')).to eq false
 end
