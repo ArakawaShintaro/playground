00283463541d24be745d41a66722dc580eb0e328
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-05 17:58:20 +0900

    add nest

--- rspec/rspec3-for-beginners/message_filter_spec.rb
+++ rspec/rspec3-for-beginners/message_filter_spec.rb
@@ -1,21 +1,26 @@
 require_relative 'message_filter'
 
-# it_behaves_like の引数は十分に説明的で、
-# 先にある shared_examples の中身を見にいかなくても済むことが望ましい
-shared_examples 'MessageFilter with argument "foo"' do
-  it { is_expected.to be_detect('hello from foo') }
-  # expect(@filter.detect?('hello from foo')).to eq true
-  it { is_expected.not_to be_detect('hello, world') }
-  # expect(@filter.detect?('hello, world!')).to eq false
-end
+describe MessageFilter do
+  # it_behaves_like の引数は十分に説明的で、
+  # 先にある shared_examples の中身を見にいかなくても済むことが望ましい
+  shared_examples 'MessageFilter with argument "foo"' do
+    it { is_expected.to be_detect('hello from foo') }
+    # expect(@filter.detect?('hello from foo')).to eq true
+    # be_predicticateは?で終わるメソッドを代用できる
+    it { is_expected.not_to be_detect('hello, world') }
+    # expect(@filter.detect?('hello, world!')).to eq false
+  end
 
-describe MessageFilter, 'with argument "foo"'  do
-  subject { MessageFilter.new('foo') }
-  it_behaves_like 'MessageFilter with argument "foo"'
-end
+  describe 'with argument "foo"'  do
+    subject { MessageFilter.new('foo') }
+    # subjectはテスト対象を引き上げることができる
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end
 
-describe MessageFilter, 'with argument "foo", "bar"' do
-  subject { MessageFilter.new('foo', 'bar') }
-  it { is_expected.to be_detect('hello from bar') }
-  it_behaves_like 'MessageFilter with argument "foo"'
+  describe 'with argument "foo", "bar"' do
+    subject { MessageFilter.new('foo', 'bar') }
+    it { is_expected.to be_detect('hello from bar') }
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end
+
 end
