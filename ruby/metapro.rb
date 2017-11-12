72fe96af555b5b0f578de68cd44262e379e85662
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-12 17:09:36 +0900

    クラスインスタンス変数

--- ruby/metapro.rb
+++ ruby/metapro.rb
@@ -12,3 +12,31 @@ end
 
 p klass_object == HogeClass
 p HogeClass.new.hello
+
+
+#クラスインスタンス変数は、継承された時には継承先では参照されない
+class InstanceCountClass
+  @instance_count = 0
+
+  def self.instance_count_up
+    @instance_count += 1 if @instance_count
+  end
+
+  def self.instance_count
+    @instance_count
+  end
+
+  def initialize
+    self.class.instance_count_up
+  end
+end
+
+class NewInstanceCountClass < InstanceCountClass
+end
+
+5.times do
+  InstanceCountClass.new
+end
+
+p InstanceCountClass.instance_count
+p NewInstanceCountClass.instance_count #nil
