f770230a17768f0f04ce3a7c8f90d56620f1b362
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-12 15:16:02 +0900

    objectの操作

--- ruby/dir.rb
+++ ruby/dir.rb
@@ -1,12 +1,18 @@
-puts Dir.entries('.')
+# puts Dir.entries('.')
 
-puts ""
+# puts ""
 
-Human = Struct.new('Human', :age, :gender) {
-  def teen?
-    (13..19).include?(age)
-  end
-  puts self
-}
+# Human = Struct.new('Human', :age, :gender) {
+#   def teen?
+#     (13..19).include?(age)
+#   end
+#   puts self
+# }
 
-Human.new(10).teen?
+# Human.new(10).teen?
+
+str = "hoge"
+p ObjectSpace._id2ref(str.object_id)
+ObjectSpace.define_finalizer(str) do
+  puts 'finalizing...'
+end
