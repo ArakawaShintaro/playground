36bc2bf5cb318d39c580d989d0db1b7c0a28d388
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-18 01:59:47 +0900

    unbind method obj

--- ruby/method_obj.rb
+++ ruby/method_obj.rb
@@ -8,3 +8,11 @@ method_obj = MethodObj.new
 p method_obj.method(:hoge).parameters
 p method_obj.method(:fuga).parameters
 p method_obj.method(:piyo).parameters
+
+#UnbounedMethod
+p Array.instance_method(:shift)
+p [].method(:shift).unbind
+
+unbind_shift = [].method(:shift).unbind
+unbind_shift.bind([1,2,3])
+p unbind_shift.bind([1,2,3]).call
