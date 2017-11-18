436ddba0ed82688200f95080c6d164389a001397
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-19 02:52:52 +0900

    class hock

--- ruby/class_and_module_hock.rb
+++ ruby/class_and_module_hock.rb
@@ -34,3 +34,23 @@ p IncludedCount.included_count
 p IncludedCount.included_module_and_class
 
 #クラス継承のフック
+
+class SuperClass
+  class << self
+    def inherited(subclass)
+      @classes ||= []
+      @classes << subclass
+    end
+
+    def subclasses
+      @classes
+    end
+  end
+end
+
+p SuperClass.subclasses
+class Sub1 < SuperClass; end
+class Sub2 < SuperClass; end
+class Sub3 < SuperClass; end
+
+p SuperClass.subclasses
