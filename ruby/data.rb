64de2797dd092dba5402fa652af3ccc3a08ad674
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-23 20:21:41 +0900

    yaml

---
+++ ruby/data.rb
@@ -0,0 +1,14 @@
+# require 'yaml'
+
+# yaml_string = <<EOS
+# ---
+# remote:
+#   host: localhost
+#   port: 22
+#   ssh: true
+#   username: user
+#   password: null
+# EOS
+
+# p setting = Yaml.load(yaml_string)
+
