c0960b8c63423ec91527e66c3b7a0026fa32e0fa
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-23 20:55:20 +0900

    csv writting

--- ruby/data.rb
+++ ruby/data.rb
@@ -45,3 +45,18 @@ converter = ->(feild, field_info) {
 }
 
 p CSV.parse(%(foo, bar, baz), converters: converter)
+
+puts
+
+puts [%w(foo bar baz) ,%w(hoge piyo fuga)].map(&:to_csv).join
+
+puts
+
+headers = %w( name age gender)
+people = [ ['alice', 14, 'female'], ['bob', 32, 'unknown'], ['carol', 17, 'female'] ]
+csv_string = CSV.generate('', write_headers: true, headers: headers) {
+  |csv| people.each do | person |
+    csv << person
+  end
+}
+puts csv_string
