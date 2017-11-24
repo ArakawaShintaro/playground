ff79d762b59f41bab4ae96bf9556ddabb59f492d
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-25 02:43:42 +0900

    redirect

--- ruby/http.rb
+++ ruby/http.rb
@@ -6,3 +6,24 @@ response = Net::HTTP.get_response(uri)
 p response.code
 p response.message
 p response.body
+
+#6回までリダイレクト先に遷移する
+def fetch(uri, limit = 6)
+  response = Net::HTTP.get_response(uri)
+
+  case response.code.to_i
+  when 200
+    puts response.body
+  when 300, 301
+    unless limit.zero?
+      fetch URI.parse(response['Location']), limit.pred
+    else
+      response.value
+    end
+  else
+    response.value
+  end
+end
+
+uri = URI.parse('http://gihyo.jp/')
+fetch uri
