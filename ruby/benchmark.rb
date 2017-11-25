cab080ed99069fe50bce97a0c37a6500b9e702d5
Arakawa Shintaro <arashinbe@gmail.com>
2017-11-26 02:29:26 +0900

    require irb

--- ruby/benchmark.rb
+++ ruby/benchmark.rb
@@ -1,4 +1,5 @@
 require 'benchmark'
+require 'irb'
 
 n = 1_000_000
 
@@ -6,6 +7,8 @@ Benchmark.bm(7, '=>total:') do |x|
   f = x.report("for:") { for i in 1..n; val = 'value' end }
   w = x.report("while:") { i = 1; while i <= n; val = 'value'; i += 1 end }
   t = x.report("times:") { n.times do val = 'value' end }
+
+  binding.irb
 
   total = f + w + t
   [total]
