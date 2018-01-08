5d7c5f029ce5cadda0d56dc12270f4e50260ea48
Arakawa Shintaro <arashinbe@gmail.com>
2018-01-08 13:49:39 +0900

    Wheelクラスに切り分けた

--- ruby-object-oriented/1_.rb
+++ ruby-object-oriented/1_.rb
@@ -1,23 +1,38 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @wheel = wheel
   end
 
   def ratio
-    chainring / cof.to_f
+    chainring / cog.to_f
   end
 
   def gear_inches
     ratio * wheel.diameter
   end
+end
 
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
   end
 end
+
+wheel = Wheel.new(26, 1.5)
+puts Gear.new(52, 11, wheel).gear_inches
+puts Gear.new(52, 11).ratio
