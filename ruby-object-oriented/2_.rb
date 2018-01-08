7d65c96b0c8c9999e1587d0100739e3e6c319951
Arakawa Shintaro <arashinbe@gmail.com>
2018-01-08 15:02:25 +0900

    依存を明確にする方法

--- ruby-object-oriented/2_.rb
+++ ruby-object-oriented/2_.rb
@@ -13,3 +13,24 @@ class Gear
 end
 
 Gear.new(10, 20, Wheel.new(25, 11)).gear_inches
+
+# もしくは、依存を明確にする
+
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+
+  def wheel
+    @wheel ||= Wheel.new(rim, tire)
+  end
+end
