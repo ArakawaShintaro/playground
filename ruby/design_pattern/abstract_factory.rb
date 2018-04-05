d5fed574b315a39f8d3ce28692f55bbae1e43eb2
Arakawa Shintaro <arashinbe@gmail.com>
2018-04-06 03:28:59 +0900

    add

--- ruby/design_pattern/abstract_factory.rb
+++ ruby/design_pattern/abstract_factory.rb
@@ -39,3 +39,49 @@ class WaterLily
     puts "スイレン #{@name} は成長中です"
   end
 end
+
+# 池の生態系を作る (Abstract Factory)
+class OrganismFactory
+  def initialize(number_animals, number_plants)
+    @animals = []
+    # 池の動物を定義する
+    number_animals.times do |i|
+      animal = new_animal("動物 #{i}")
+      @animals << animal
+    end
+    @plants = []
+    # 池の植物を定義する
+    number_plants.times do |i|
+      plant = new_plant("植物 #{i}")
+      @plants << plant
+    end
+  end
+  # 植物についてのオブジェクトを返す
+  def get_plants
+    @plants
+  end
+  # 動物についてのオブジェクトを返す
+  def get_animals
+    @animals
+  end
+end
+# カエル(Frog)と藻(Algae)の生成を行う (Concrete Factory)
+class FrogAndAlgaeFactory < OrganismFactory
+  private
+  def new_animal(name)
+    Frog.new(name)
+  end
+  def new_plant(name)
+    Algae.new(name)
+  end
+end
+# アヒル(Duck)とスイレン(WaterLily)の生成を行う(Concrete Factory)
+class DuckAndWaterLilyFactory < OrganismFactory
+  private
+  def new_animal(name)
+    Duck.new(name)
+  end
+  def new_plant(name)
+    WaterLily.new(name)
+  end
+end
