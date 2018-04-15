9b0e0fc8dec1e046db2ae1db19eb471acad56fa3
Arakawa Shintaro <arashinbe@gmail.com>
2018-04-16 01:40:43 +0900

    add salt water

--- ruby/design_pattern/builder.rb
+++ ruby/design_pattern/builder.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # ConcreteBuilder（具体ビルダ）
 class SugarWater
   attr_accessor :water, :sugar
@@ -6,24 +7,41 @@ class SugarWater
     @water = water
     @sugar = sugar
   end
+
+  def add_material(sugar_amount)
+    @sugar += sugar_amount
+  end
+end
+
+class SaltWater
+  attr_accessor :water, :salt
+
+  def initialize(water, salt)
+    @water = water
+    @salt = salt
+  end
+
+  def add_material(salt_amount)
+    @salt += salt_amount
+  end
 end
 
 # Builder
-class SugarWaterBuilder
-  def initialize
-    @sugar_water = SugarWater.new(0, 0)
+class WaterWithMaterialBuilder
+  def initialize(class_name)
+    @water_with_material = class_name.new(0, 0)
   end
 
-  def add_sugar(sugar_amount)
-    @sugar_water.sugar += sugar_amount
+  def add_material(material_amount)
+    @water_with_material.add_material(material_amount)
   end
 
   def add_water(water_amount)
-    @sugar_water.water += water_amount
+    @water_with_material.water += water_amount
   end
 
   def result
-    @sugar_water
+    @water_with_material
   end
 end
 
@@ -34,13 +52,18 @@ class Director
 
   def cook
     @builder.add_water(150)
-    @builder.add_sugar(50)
+    @builder.add_material(50)
     @builder.add_water(200)
-    @builder.add_sugar(30)
+    @builder.add_material(30)
   end
 end
 
-builder = SugarWaterBuilder.new
+builder = WaterWithMaterialBuilder.new(SugarWater)
+director = Director.new(builder)
+director.cook
+p builder.result
+
+builder = WaterWithMaterialBuilder.new(SaltWater)
 director = Director.new(builder)
 director.cook
 p builder.result
