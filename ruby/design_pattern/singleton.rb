cca028dbb047c37698d27eb95bde7c39fe205cf9
Arakawa Shintaro <arashinbe@gmail.com>
2018-04-15 12:18:33 +0900

    add singleton

---
+++ ruby/design_pattern/singleton.rb
@@ -0,0 +1,19 @@
+require 'singleton'
+require 'pry'
+
+class SingletonObject
+  include Singleton
+  attr_accessor :counter
+
+  def initialize
+    @counter = 0
+  end
+end
+
+obj1 = SingletonObject.instance
+obj1.counter += 1
+
+obj2 = SingletonObject.instance
+puts obj2.counter
+# instanceが引き継がれる
+# newはerrorになる
